7cf674209354444a01615f0e71a9618b1774dd28
Xin Zhengfang <xinzhengfang@qq.com>
2019-11-13 17:55:40 +0800

    fine adjust

--- feature_visual.py
+++ feature_visual.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
-data = pd.read_csv('feature_record_hgnn.csv',header=None)
+data = pd.read_csv('feature_record_egnn.csv',header=None)
 for i in range(data.shape[0]):
     X_ls = []
     if data.iloc[i,0] == 'label':
@@ -28,11 +28,10 @@ for i in range(data.shape[0]):
         prep_value = int(label.shape[0]/5)-1 # trand -1 / non -0
         X_embedded = TSNE(n_components=2,perplexity=5).fit_transform(X_np)
         plt.figure(i)
-        p1 = plt.scatter(X_embedded[:-5,0], X_embedded[:-5,1], c=label[:-5])
-        p2 = plt.scatter(X_embedded[-5:,0],X_embedded[-5:,1],c=label[-5:],marker='*')
+        p1 = plt.scatter(X_embedded[:-5,0], X_embedded[:-5,1], c=label[:-5],s=50)
+        p2 = plt.scatter(X_embedded[-5:,0],X_embedded[-5:,1],c=label[-5:],marker='x',s=50)
+        xmin, xmax, ymin, ymax = plt.axis()
         plt.legend((p1, p2), ('support','query'))
-        plt.title(visual_name)
-        plt.xlabel('x')
-        plt.ylabel('y')
+        plt.axis([1.5*xmin, 1.5*xmax, 1.5*ymin, 1.5*ymax])
 plt.show()
 
